bea1af9b89ed3a54cda710e73ce8161a17da9853
MatiasBaezaGraf <matiasbaezagraf@gmail.com>
2022-02-20 17:23:18 -0300

    Login y Register

--- db/schema.rb
+++ db/schema.rb
@@ -58,10 +58,10 @@ ActiveRecord::Schema.define(version: 2022_01_29_140654) do
     t.string "name"
     t.float "price"
     t.integer "stock"
+    t.integer "state"
     t.datetime "registration_date", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "state"
     t.integer "max_stock"
   end
 
